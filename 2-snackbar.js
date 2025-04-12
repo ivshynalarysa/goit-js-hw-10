56af7937ca1d1842352d9b0c4c60236478cc4ad0
ivshynalarysa <ivshynalarysa@gmail.com>
2025-04-12 15:05:35 +0000

    Deploying to gh-pages from @ ivshynalarysa/goit-js-hw-10@90e31d88d5b775f0ea37f34042adeb5f227dd2cb 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-D9tUk8Q0.js";`✅ Fulfilled promise in ${delay}ms``❌ Rejected promise in ${delay}ms`;
+//# sourceMappingURL=2-snackbar.js.map
