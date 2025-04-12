cf91b1957f985bd9938d836062c467a64e88ad45
ivshynalarysa <ivshynalarysa@gmail.com>
2025-04-12 15:52:01 +0100

    test2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,11 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
+`✅ Fulfilled promise in ${delay}ms`
+
+`❌ Rejected promise in ${delay}ms`
+
+
