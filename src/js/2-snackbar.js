28e8e2ac4b480774629139c561967fd824a0f98e
ivshynalarysa <ivshynalarysa@gmail.com>
2025-04-12 18:13:30 +0100

    modified css files

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,8 +4,31 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 
-`✅ Fulfilled promise in ${delay}ms`
+const form = document.querySelector('.form')
 
-`❌ Rejected promise in ${delay}ms`
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event){
+    event.preventDefault()
+    const {delay, state} = event.currentTarget.elements
+    createPromise(delay.value, state.value)
+    .then(data => iziToast.success({position: "topRight", message: `✅ Fulfilled promise in ${data}ms`}))
+    .catch(data => iziToast.error({position: "topRight", message: `❌ Rejected promise in ${data}ms`}))
+    form.reset()
+}
+
+function createPromise(delay, state){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (state === 'fulfilled') {
+            resolve(delay);
+          } else {
+            reject(delay);
+          }
+        }, delay);
+      });
+}
+
+
 
 
